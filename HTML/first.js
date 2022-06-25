29bd10d71a652716cd7783378c7428335c6e32b5
PRAGATI <pragati2004muz@gmail.com>
2022-06-25 11:27:28 +0530

    main

---
+++ HTML/first.js
@@ -0,0 +1,45 @@
+var a=true;
+var b=true;
+var c=false;
+
+a+b = 2
+
+var a = "10";
+var b = "2";
+
+>>> parseFloat()/ parseInt()
+
+var a = "10";
+var b = "2";
+parseInt(a)+parseInt(b)
+
+var a = "Hiiii"
+parseInt(a)
+
+var a = "Delhi1234"
+var b = "Patna123"
+parseInt(a);
+parseInt(b);
+
+var name = 'Techvanto'
+var name1 = "Academy"
+var rating = 4.5
+var source = "Delhi"
+var country = "India"
+
+// es5 way--->
+
+var out = "The "+name+" "+name1+" 0f" + source+ " " + country + "is having rating" + rate
+
+
+// es5 way--->
+
+var out = "The "+name+" "+name1+" 0f" + source+ " " + country + "is having rating" + rating + "."
+
+
+
+
+
+
+
+
